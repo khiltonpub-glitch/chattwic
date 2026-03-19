776f13f66ab57cd2e0fee3b28dac859b113db59e
khiltonpub-glitch <k.hiltonpub@gmail.com>
2026-03-19 00:32:29 -0400

    Add files via upload

---
+++ sections/Cities.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function Cities() {
+  return (
+    <section>
+      <h2>Cities</h2>
+      <p>The new South. A city rebuilt on progress and diversity.</p>
+    </section>
+  )
+}
